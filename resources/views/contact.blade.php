a75cc31889e2b217c90fde757eed60ce99574fbc
Masara-james <masarajames02@gmail.com>
2022-03-10 10:27:21 +0300

    Embedeing map

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,8 +5,7 @@
 
 <div class="row">
 
-    <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30153.788252261566!2d72.82321484621745!3d19.141690214227783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b63aceef0c69%3A0x2aa80cf2287dfa3b!2sJogeshwari%20West%2C%20Mumbai%2C%20Maharashtra%20400047!5e0!3m2!1sen!2sin!4v1632137920043!5m2!1sen!2sin" allowfullscreen="" loading="lazy"></iframe>
-
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15958.888570710844!2d36.9609217!3d-0.3978314!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7ae4dae9615396a6!2sDEDAN%20KIMATHI%20UNIVERSITY%20OF%20TECHNOLOGY!5e0!3m2!1sen!2ske!4v1646897161142!5m2!1sen!2ske" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
     <form action="">
         <h3>get in touch</h3>
         <input type="text" placeholder="your name" class="box">
